e09c87868b67ba6dc974ec9c8d86cea67ead6926
theofilius <136842438+theofilius@users.noreply.github.com>
2026-02-04 20:06:34 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000107586_Theofilius/week01/GameStore.kt
+++ src/oop_00000107586_Theofilius/week01/GameStore.kt
@@ -4,3 +4,9 @@ fun main() {
     val gameTitle = "Fc 26"
     val price = 750000
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000)
+        (price * 20) / 100
+else
+        (price *10) / 100
