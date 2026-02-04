f58a95e00854a1ffb16f0d4848ca9716eca104e8
theofilius <136842438+theofilius@users.noreply.github.com>
2026-02-04 15:59:39 +0700

    week01: add calculateStatus function

--- src/oop_00000107586_Theofilius/week01/GradingSystem.kt
+++ src/oop_00000107586_Theofilius/week01/GradingSystem.kt
@@ -14,4 +14,7 @@ fun main() {
         else -> "D"
     }
     println("Grade kamu: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
+
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
