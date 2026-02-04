4c06cc78822296db6f0586e49c572d8c725fa4bf
theofilius <136842438+theofilius@users.noreply.github.com>
2026-02-04 15:56:54 +0700

    week01: implement grading logic using when expression

--- src/oop_00000107586_Theofilius/week01/GradingSystem.kt
+++ src/oop_00000107586_Theofilius/week01/GradingSystem.kt
@@ -1,9 +1,17 @@
 package oop_00000107586_Theofilius.week01
 
+
 fun main() {
     var name = "John Thor"
     var score = 80
 
     // Concatenation gaya lama
     println("Nama: $name, Nilai: $score")
+    val grade = when (score) {
+        in 90.. 100 -> "A"
+        in 80.. 89 -> "B"
+        in 70.. 79 -> "C"
+        else -> "D"
+    }
+    println("Grade kamu: $grade")
 }
