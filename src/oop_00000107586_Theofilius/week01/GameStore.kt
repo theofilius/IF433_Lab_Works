b70de364396c813d122f298dd1207f1de5f9e499
theofilius <136842438+theofilius@users.noreply.github.com>
2026-02-04 20:12:23 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000107586_Theofilius/week01/GameStore.kt
+++ src/oop_00000107586_Theofilius/week01/GameStore.kt
@@ -3,6 +3,11 @@ package oop_00000107586_Theofilius.week01
 fun main() {
     val gameTitle = "Fc 26"
     val price = 750000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+
+    printReciept(title = gameTitle, finalPrice = finalPrice)
 }
 
 fun calculateDiscount(price: Int): Int =
@@ -10,3 +15,9 @@ fun calculateDiscount(price: Int): Int =
         (price * 20) / 100
 else
         (price *10) / 100
+
+fun printReciept(title: String, finalPrice: Int) {
+    println("====Game Store Reciept====")
+    println("Game: $title")
+    println("Final Price: Rp $finalPrice")
+}
