1bfa419fdf4555f237dcad7ab04ece2eaea69155
theofilius <136842438+theofilius@users.noreply.github.com>
2026-02-04 15:39:27 +0700

    week01: refactor to use val and string templates

--- src/oop_00000107586_Theofilius/week01/GradingSystem.kt
+++ src/oop_00000107586_Theofilius/week01/GradingSystem.kt
@@ -1,9 +1,9 @@
 package oop_00000107586_Theofilius.week01
 
 fun main() {
-    var name: String = "John Thor"
-    var score: Int = 80
+    var name = "John Thor"
+    var score = 80
 
     // Concatenation gaya lama
-    println("Nama: " + name + ", Nilai: " + score)
+    println("Nama: $name, Nilai: $score")
 }
