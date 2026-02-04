044ef8568370dcdede7cefbb0e095681178f8f6c
theofilius <136842438+theofilius@users.noreply.github.com>
2026-02-04 15:34:33 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000107586_Theofilius/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000107586_Theofilius.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
