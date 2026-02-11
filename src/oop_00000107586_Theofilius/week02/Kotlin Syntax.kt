e24d7b21336b5b88cd2072c2a6b86864f0824924
theofilius <136842438+theofilius@users.noreply.github.com>
2026-02-11 15:45:09 +0700

    week02: add warning validation in init block

--- src/oop_00000107586_Theofilius/week02/Kotlin Syntax.kt
+++ src/oop_00000107586_Theofilius/week02/Kotlin Syntax.kt
@@ -1,6 +1,13 @@
 package oop_00000107586_Theofilius.week02
 
-class Student (val name:String, val nim:Int, var major:String ){
-
+class Student (val name:String, val nim:String, var major:String ){
+    init {
+        if (nim.length !=5) {
+            println("WARNING: Object tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermaslaah di sistem.")
+        } else {
+            println("LOG: Object Student $name berhasilkan dialokasikan di Memory.")
+        }
+    }
   
 }
