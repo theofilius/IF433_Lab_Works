5ef50973dfa3cca528626b8bbba13ff154e8b63c
theofilius <136842438+theofilius@users.noreply.github.com>
2026-02-18 15:19:53 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000107586_Theofilius/week03/Employee.kt
+++ src/oop_00000107586_Theofilius/week03/Employee.kt
@@ -1,5 +1,8 @@
 package oop_00000107586_Theofilius.week03
 
 class Employee(val name: String) {
-    var salaru: Int = 0
+    var salary: Int = 0
+    println("Mencoba set gaji ke: $value")
+
+    this.salary = value
 }
