c8ad3119b308130878962ce1deee0c93088ce99f
theofilius <136842438+theofilius@users.noreply.github.com>
2026-02-25 14:51:59 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_00000107586_Theofilius/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_00000107586_Theofilius.week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
