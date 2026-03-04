96beabcd90ffcc7cabaecf73b1ad459ec65b1e62
theofilius <136842438+theofilius@users.noreply.github.com>
2026-03-04 15:28:12 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000107586_Theofilius/week05/MathHelper.kt
@@ -0,0 +1,15 @@
+package oop_00000107586_Theofilius.week05
+
+class MathHelper {
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
