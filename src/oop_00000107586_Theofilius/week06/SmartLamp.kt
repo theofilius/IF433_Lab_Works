d541d1c4b5859a19ea367bc08c5f9c29942df17a
theofilius <136842438+theofilius@users.noreply.github.com>
2026-03-11 15:45:36 +0700

    week06: (task) create SmartLamp

---
+++ src/oop_00000107586_Theofilius/week06/SmartLamp.kt
@@ -0,0 +1,16 @@
+package oop_00000107586_Theofilius.week06
+
+class SmartLamp(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable {
+
+    override fun turnOn() {
+        println("Lampu \"$name\" dinyalakan.")
+    }
+
+    override fun turnOff() {
+        println("Lampu \"$name\" dimatikan.")
+    }
+}
+
