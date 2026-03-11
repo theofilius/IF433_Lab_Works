e080709d61ae1d7a1e35b39fa3566e43c2cfb3e0
theofilius <136842438+theofilius@users.noreply.github.com>
2026-03-11 15:34:57 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_00000107586_Theofilius/week06/Smartphone.kt
+++ src/oop_00000107586_Theofilius/week06/Smartphone.kt
@@ -1,5 +1,10 @@
 package oop_00000107586_Theofilius.week06
 //commit ulang karena salah tulis commit message
 class Smartphone : Camera, Phone {
+    override fun turnOn() {
+        super<Camera>.turnOn()
+        super<Phone>.turnOn()
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
 
 }
