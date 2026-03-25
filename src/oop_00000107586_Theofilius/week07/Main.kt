fd48d0e8ac064789856361ddd0a94db3205d9158
theofilius <136842438+theofilius@users.noreply.github.com>
2026-03-25 15:51:02 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_00000107586_Theofilius/week07/Main.kt
+++ src/oop_00000107586_Theofilius/week07/Main.kt
@@ -25,6 +25,8 @@ fun main() {
     val uiMessage = when(response) {
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        ApiResponse.Loading -> "Tampilkan Spinner"
     }
 
+    println(uiMessage)
 }
