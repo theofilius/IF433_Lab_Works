7bbcb3af8d779d20378fe25905eb05e738bafe1a
theofilius <136842438+theofilius@users.noreply.github.com>
2026-03-25 15:59:05 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/oop_00000107586_Theofilius/week07/BattleEvent.kt
@@ -0,0 +1,9 @@
+package oop_00000107586_Theofilius.week07
+
+sealed class BattleState {
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+    data class LootDropped(val item: GameItem) : BattleState()
+    data class GameOver(val reason: String) : BattleState()
+    object SafeZone : BattleState()
+}
+
