429d7ecdec451675e14a08d2307c04b38133cd6f
theofilius <136842438+theofilius@users.noreply.github.com>
2026-03-25 15:41:58 +0700

    week07: implement copy and destructuring on data class

--- src/oop_00000107586_Theofilius/week07/Main.kt
+++ src/oop_00000107586_Theofilius/week07/Main.kt
@@ -12,6 +12,10 @@ fun main() {
     println("\n=== TEST REGULAR CLASSES ===")
     val data1 = DataUser("Alice", 22)
     val data2 = DataUser("Alice", 22)
+    val data3 = data1.copy(age = 23)
+    println("Hasil Copy: $data3")
+
+    val (userName, userAge) = data1
     println(data1)
     println("Sama? ${data1 == data2}")
 
