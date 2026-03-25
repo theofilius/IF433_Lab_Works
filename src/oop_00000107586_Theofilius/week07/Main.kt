57b30ccae4c4ee2c7b0426ff577cdabe60a84bb5
theofilius <136842438+theofilius@users.noreply.github.com>
2026-03-25 15:40:16 +0700

    week07: implement data class for structural equality

--- src/oop_00000107586_Theofilius/week07/Main.kt
+++ src/oop_00000107586_Theofilius/week07/Main.kt
@@ -10,9 +10,9 @@ fun main() {
     client.connect()
 
     println("\n=== TEST REGULAR CLASSES ===")
-    val reg1 = RegularUser("Alice", 22)
-    val reg2 = RegularUser("Alice", 22)
-    println(reg1)
-    println("Sama? ${reg1 == reg2}")
+    val data1 = DataUser("Alice", 22)
+    val data2 = DataUser("Alice", 22)
+    println(data1)
+    println("Sama? ${data1 == data2}")
 
 }
--- src/oop_00000107586_Theofilius/week07/UserModels.kt
+++ src/oop_00000107586_Theofilius/week07/UserModels.kt
@@ -1,4 +1,5 @@
 package oop_00000107586_Theofilius.week07
 
-class RegularUser(val name: String, val age: Int){
-}
+class RegularUser(val name: String, val age: Int)
+
+data class DataUser(val name: String, val age: Int)
