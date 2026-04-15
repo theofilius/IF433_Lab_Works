ad0b420286f5a8657d8435f9f85915958a375545
theofilius <136842438+theofilius@users.noreply.github.com>
2026-04-15 15:19:13 +0700

    week08: implement chained safe calls and elvis

---
+++ src/oop_00000107586_Theofilius/week08/Main.kt
@@ -0,0 +1,9 @@
+package oop_00000107586_Theofilius.week08
+
+fun main() {
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota tidak diketahui"
+    println("Tujuan pengiriman: $destination")
+}
