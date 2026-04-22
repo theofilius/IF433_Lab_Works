602e2e521ccb5ed2ed3abe595dcd7576b7353b42
theofilius <136842438+theofilius@users.noreply.github.com>
2026-04-22 15:45:17 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000107586_Theofilius/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_00000107586_Theofilius.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
+
