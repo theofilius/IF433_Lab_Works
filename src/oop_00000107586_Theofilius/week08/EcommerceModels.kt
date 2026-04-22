6c9e2a39e65ffc6ed8000b14ee6f858bef019586
theofilius <136842438+theofilius@users.noreply.github.com>
2026-04-22 15:43:30 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_00000107586_Theofilius/week08/EcommerceModels.kt
@@ -0,0 +1,19 @@
+package oop_00000107586_Theofilius.week08
+
+sealed class Product {
+    abstract val id: String
+    abstract val name: String
+}
+
+data class Electronic(
+    override val id: String,
+    override val name: String,
+    val warrantyMonths: Int,
+) : Product()
+
+data class Clothing(
+    override val id: String,
+    override val name: String,
+    val size: String,
+) : Product()
+
