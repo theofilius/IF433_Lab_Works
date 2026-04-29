0b3c1e24cbde6f6ef3308330b9ecd00b9cfa25ec
theofilius <136842438+theofilius@users.noreply.github.com>
2026-04-29 14:55:46 +0700

    week09: implement map for key-value storage

--- src/oop_00000107586_Theofilius/week09/CollectionsBasics.kt
+++ src/oop_00000107586_Theofilius/week09/CollectionsBasics.kt
@@ -9,10 +9,19 @@ fun main() {
     val uniqueFromList = immutableList.toSet()
     println("Unique elements (set): $uniqueFromList")
 
+    // Map (key-value)
+    val priceMap = mapOf(
+        "BTC" to 65000,
+        "ETH" to 3500,
+        "SOL" to 150
+    )
+    println("Map: $priceMap")
+    println("BTC price: ${priceMap["BTC"]}")
+
     // Mutable List (can be modified)
     val mutableList = mutableListOf("AAPL", "TSLA", "NVDA")
     println("Mutable list (before): $mutableList")
-//
+
     mutableList.add("MSFT")
     mutableList.remove("TSLA")
     println("Mutable list (after): $mutableList")
