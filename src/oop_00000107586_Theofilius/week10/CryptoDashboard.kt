3f86d1c26c8e825ed44e650dc4f13156d433f7e6
theofilius <136842438+theofilius@users.noreply.github.com>
2026-04-29 15:37:57 +0700

    week10: (task) populate coin repository

--- src/oop_00000107586_Theofilius/week10/CryptoDashboard.kt
+++ src/oop_00000107586_Theofilius/week10/CryptoDashboard.kt
@@ -2,5 +2,8 @@ package oop_00000107586_Theofilius.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
-}
 
+    coinRepo.add(Coin("BTC", 0.025))
+    coinRepo.add(Coin("ETH", 1.5))
+    coinRepo.add(Coin("USDT", 250.0))
+}
