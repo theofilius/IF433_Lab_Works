3eececcdf62fea166b8d6dd56465f67d9e07943a
theofilius <136842438+theofilius@users.noreply.github.com>
2026-04-29 15:34:58 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000107586_Theofilius/week10/WalletRepository.kt
@@ -0,0 +1,12 @@
+package oop_00000107586_Theofilius.week10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> = items.toList()
+}
+
