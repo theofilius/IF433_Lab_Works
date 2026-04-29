821b3c5cf31f7b6e58b73c7a97087aec74a58c06
theofilius <136842438+theofilius@users.noreply.github.com>
2026-04-29 14:59:28 +0700

    week09: implement filter HOF to extract data

---
+++ src/oop_00000107586_Theofilius/week09/HigherOrderFunctions.kt
@@ -0,0 +1,9 @@
+package oop_00000107586_Theofilius.week09
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    val evenNumbers = numbers.filter { it % 2 == 0 }
+    println("Even numbers: $evenNumbers")
+}
+
