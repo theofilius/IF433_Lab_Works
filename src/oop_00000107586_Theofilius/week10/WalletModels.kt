27d2b19405b716e3b61da1280d9b497b50f0eeb2
theofilius <136842438+theofilius@users.noreply.github.com>
2026-04-29 15:34:33 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000107586_Theofilius/week10/WalletModels.kt
@@ -0,0 +1,12 @@
+package oop_00000107586_Theofilius.week10
+
+data class Coin(
+    val name: String,
+    val balance: Double
+)
+
+data class Transaction(
+    val id: String,
+    val amount: Double
+)
+
