6e15348efc2edd3b2884ecd8acfc660d36293048
theofilius <136842438+theofilius@users.noreply.github.com>
2026-04-29 15:38:36 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000107586_Theofilius/week10/CryptoDashboard.kt
+++ src/oop_00000107586_Theofilius/week10/CryptoDashboard.kt
@@ -6,4 +6,6 @@ fun main() {
     coinRepo.add(Coin("BTC", 0.025))
     coinRepo.add(Coin("ETH", 1.5))
     coinRepo.add(Coin("USDT", 250.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
