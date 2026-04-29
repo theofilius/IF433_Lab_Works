fa1d5e5578ae4ee98a6fc74950749b465c47f392
theofilius <136842438+theofilius@users.noreply.github.com>
2026-04-29 14:50:39 +0700

    week09: create immutable and mutable list

---
+++ src/oop_00000107586_Theofilius/week09/CollectionsBasics.kt
@@ -0,0 +1,16 @@
+package oop_00000107586_Theofilius.week09
+
+fun main() {
+    // Immutable List (read-only)
+    val immutableList = listOf("BTC", "ETH", "SOL", "BTC")
+    println("Immutable list: $immutableList")
+
+    // Mutable List (can be modified)
+    val mutableList = mutableListOf("AAPL", "TSLA", "NVDA")
+    println("Mutable list (before): $mutableList")
+
+    mutableList.add("MSFT")
+    mutableList.remove("TSLA")
+    println("Mutable list (after): $mutableList")
+}
+
