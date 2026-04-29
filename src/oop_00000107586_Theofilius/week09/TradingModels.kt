3df3fb1761a14b6dcd83c4dabb67f4296cf235d8
theofilius <136842438+theofilius@users.noreply.github.com>
2026-04-29 15:02:31 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000107586_Theofilius/week09/TradingModels.kt
@@ -0,0 +1,10 @@
+package oop_00000107586_Theofilius.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val roe: Double,
+    val leverage: Int,
+    val status: String
+)
+
