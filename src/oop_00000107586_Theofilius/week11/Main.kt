b3c1fe2e172c4352621e58225a5f7c31a18dcce8
theofilius <136842438+theofilius@users.noreply.github.com>
2026-05-06 14:38:03 +0700

    week11: test extension functions in main

---
+++ src/oop_00000107586_Theofilius/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000107586_Theofilius.week11
+
+fun main () {
+    println("=== TEST EXTENNSION FUNCTION ===")
+    println("Alex".addGreeting())
+    println("Hi".repeat(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
