6f2798e669991f3f6b28e95061c588672621caa2
theofilius <136842438+theofilius@users.noreply.github.com>
2026-05-06 15:04:05 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_00000107586_Theofilius/week11/SmartHomeDashboard.kt
+++ src/oop_00000107586_Theofilius/week11/SmartHomeDashboard.kt
@@ -9,4 +9,14 @@ fun main() {
         isOnline = true
         powerLoad = 12
     }.also { homeDevices.add(it) }
+
+    SmartDevice("Ezviz Outdoor", "Camera")
+        .apply {
+            isOnline = true
+            powerLoad = 5
+        }
+        .also {
+            println("(LOG) Kamera terhubung")
+            homeDevices.add(it)
+        }
 }
