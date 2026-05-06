e37908571a481b3f27d7e8a395775974c0183281
theofilius <136842438+theofilius@users.noreply.github.com>
2026-05-06 14:59:56 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000107586_Theofilius/week11/SmartHomeModels.kt
@@ -0,0 +1,9 @@
+package oop_00000107586_Theofilius.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
+
