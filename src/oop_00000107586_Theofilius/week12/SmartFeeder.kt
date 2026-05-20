1f0099463140a076e3757c1253b8a70d70d193db
theofilius <136842438+theofilius@users.noreply.github.com>
2026-05-20 13:07:22 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_00000107586_Theofilius/week12/SmartFeeder.kt
+++ src/oop_00000107586_Theofilius/week12/SmartFeeder.kt
@@ -49,5 +49,8 @@ fun main() {
             availableGram = 1000,
             isJammed = false
         )
+    }.onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
     }
 }
