4796ea02a976950baeba86d33403cadd475bb86e
theofilius <136842438+theofilius@users.noreply.github.com>
2026-05-20 14:00:32 +0700

    week13: (task) wrap fromCsvTrade in try-catch for NumberFormatException

--- src/oop_00000107586_Theofilius/week13/CryptoTradeLogger.kt
+++ src/oop_00000107586_Theofilius/week13/CryptoTradeLogger.kt
@@ -14,12 +14,17 @@ data class TradeRecord(
 fun TradeRecord.toCsv(): String = "$id,$symbol,$type,$margin,$pnl"
 
 fun fromCsvTrade(line: String): TradeRecord? {
-    val parts = line.split(",")
-    return TradeRecord(
-        parts[0].toInt(),
-        parts[1],
-        parts[2],
-        parts[3].toDouble(),
-        parts[4].toDouble()
-    )
+    return try {
+        val parts = line.split(",")
+        TradeRecord(
+            parts[0].toInt(),
+            parts[1],
+            parts[2],
+            parts[3].toDouble(),
+            parts[4].toDouble()
+        )
+    } catch (e: Exception) {
+        println("(Log) Data korup diabaikan: $line")
+        null
+    }
 }
