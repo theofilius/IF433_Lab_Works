92a7b013e2559de3e98910c19a08f8f4eaef4edc
theofilius <136842438+theofilius@users.noreply.github.com>
2026-05-20 13:06:22 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_00000107586_Theofilius/week12/SmartFeeder.kt
+++ src/oop_00000107586_Theofilius/week12/SmartFeeder.kt
@@ -11,5 +11,10 @@ fun dispenseKibble(
         throw DispenserJamException()
     }
 
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    println("Kibble berhasil dikeluarkan!")
     return availableGram - requestedGram
 }
