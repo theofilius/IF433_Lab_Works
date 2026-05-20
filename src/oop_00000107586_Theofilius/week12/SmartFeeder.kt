2a99c154cbc3a84cc1dc0c381cb7536ed56deb0e
theofilius <136842438+theofilius@users.noreply.github.com>
2026-05-20 13:05:59 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000107586_Theofilius/week12/SmartFeeder.kt
@@ -0,0 +1,12 @@
+package oop_00000107586_Theofilius.week12
+
+fun dispenseKibble(
+    requestedGram: Int,
+    availableGram: Int,
+    isJammed: Boolean
+): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+
+    return availableGram - requestedGram
+}
+
