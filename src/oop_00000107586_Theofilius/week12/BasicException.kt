ed295fc66db9fae50df824281498ed0b202cf76c
theofilius <136842438+theofilius@users.noreply.github.com>
2026-05-20 13:02:21 +0700

    week12: create basic try-catch-finally

---
+++ src/oop_00000107586_Theofilius/week12/BasicException.kt
@@ -0,0 +1,13 @@
+package oop_00000107586_Theofilius.week12
+
+fun divide(a: Int, b: Int): Int {
+    try {
+        return a / b
+    } catch (e: ArithmeticException) {
+        println("Error: ${e.message}")
+        return -1
+    } finally {
+        println("Division attempt finished")
+    }
+}
+
